69720dec9ef9e796e497dec032e0ca55994eaf55
annagoetz1 <annalgoetz1@gmail.com>
2023-08-03 15:24:58 -0400

    Create initial computer choice

---
+++ script.js
@@ -0,0 +1,19 @@
+#create a function called getComputerChoice that randomly returns Rock Paper or Scissors
+#create a function that plays a single round 
+#create a prompt to get input from user
+#set parameters under playerSelection and computerSelection
+#return a string that declares the winner of the round
+#include case -insensitive parameters for functions
+#ensure results are returned instead of console.log
+#create a new function called game to use previous function inside of
+#make this function call 5 times over to make a 5 round game
+
+function getComputerChoice () {
+const choices = ['Rock', 'Paper', 'Scissors'];
+const randomIndex = Math.floor(Math.random() * choices.length);
+return choices[randomIndex]
+}
+ const computerSelection = getComputerChoice();
+ console.log('Computer chose: ' + computerSelection);
+
+ function 
