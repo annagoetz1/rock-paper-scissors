756cbd51733557502e3957ddcfb45c3f63a2cbeb
annagoetz1 <annalgoetz1@gmail.com>
2023-10-02 19:20:32 -0400

    Add callbacks to event listeners

--- script.js
+++ script.js
@@ -1,58 +1,80 @@
 
-//signals computer to make a random choice between rock, paper and scissors
+
 function getComputerChoice() {
   const choices = ['Rock', 'Paper', 'Scissors'];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
 
-//main game function for each round played to determine each winner
+
 function playRound(playerSelection, computerSelection) {
+
+
   if (playerSelection === computerSelection) {
     return "It's a Tie!";
   } else if (
-    (playerSelection === 'rock' && computerSelection === 'Scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'Rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'Paper')
+    (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+    (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+    (playerSelection === 'Scissors' && computerSelection === 'Paper')
   ) {
     return "You Win!";
   } else {
     return "Computer Wins!";
-  }
+  }  
 }
 
-//this function starts off our 5 round game function but allows the initial round to be included in scorekeeping
-function game() {
+
+  function updateResult(result) {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = 'Result: ' + result;
+  }
+  
+  function getPlayerSelection(callback) {
+
+    let selection = '';
+  
+    const rockButton = document.getElementById('Rock');
+    const paperButton = document.getElementById('Paper');
+    const scissorsButton = document.getElementById('Scissors');
+  
+    rockButton.addEventListener('click', () => {
+      selection = 'Rock';
+      callback(playerSelection);
+    });
+  
+    paperButton.addEventListener('click', () => {
+      selection = 'Paper';
+      callback(playerSelection);
+    });
+  
+    scissorsButton.addEventListener('click', () => {
+      selection = 'Scissors';
+      callback(playerSelection);
+    });
+  
+  }
+  getPlayerSelection();
+
+function game(playerSelection) {
+
   let playerScore = 0;
   let computerScore = 0;
 
-  
-  const initialPlayerSelection = prompt("Round 1: Rock, Paper, or Scissors? Best out of 5!").toLowerCase();
-  const initialComputerSelection = getComputerChoice();
-  const initialResult = playRound(initialPlayerSelection, initialComputerSelection);
-  alert(initialResult);
-//add thsi additional else if to ensure the player and computer scores are accurately tracked at the end
+  for (let round = 0; round < 5; round++) {
+
+ 
+    const computerSelection = getComputerChoice();
+  const result = playRound(playerSelection, computerSelection);
+
   if (result === "You Win!") {
     playerScore++;
   } else if (result === "Computer Wins!") {
     computerScore++;
   }
-
- //start this game function at round 2 to eliminate accidnetal extra rounds
-  for (let round = 2; round <= 5; round++) {
-    const playerSelection = prompt(`Round ${round}: Rock, Paper, or Scissors?`).toLowerCase();
-    const computerSelection = getComputerChoice();
-
-    const result = playRound(playerSelection, computerSelection);
-
-    if (result === "You Win!") {
-      playerScore++;
-    } else if (result === "Computer Wins!") {
-      computerScore++;
-    }
-    alert(result);
+  updateResult(result);
+  alert(result);
   }
-
+ 
   alert(`Final Scores: Player ${playerScore} - ${computerScore} Computer`);
 
   if (playerScore > computerScore) {
@@ -64,4 +86,3 @@ function game() {
   }
 }
 
-game();
